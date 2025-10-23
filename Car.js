760012af7c729633112799d929f0ffa7c236abe9
zxyn-86 <144375040+zxyn-86@users.noreply.github.com>
2025-10-23 17:06:41 +0100

    initial commit

---
+++ Car.js
@@ -0,0 +1,27 @@
+class Car
+{
+    constructor(x,y,width,height)
+    {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+
+        this.controls = new Controls();
+    }
+
+    //this basically draws the car
+    draw(ctx)
+    {
+        ctx.beginPath();
+        ctx.rect(
+            this.x-this.width/2,
+            this.y-this.height/2,
+            this.width,
+            this.height
+        );
+        
+
+        ctx.fill();
+    }
+}
